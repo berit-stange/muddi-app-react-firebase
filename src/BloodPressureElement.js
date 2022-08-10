fcc02aa2a47ccfce2eb8d0e6eca77bd939f722ea
berit-stange <76659950+berit-stange@users.noreply.github.com>
2022-08-10 20:28:46 +0200

    created BloodPressureElement.js

---
+++ src/BloodPressureElement.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { useState } from "react";
+import { db } from './firebase-config';
+import {
+    doc,
+    deleteDoc,
+    updateDoc
+} from 'firebase/firestore';
+import BloodPressureModal from './BloodPressureModal';
+
+
+const BloodPressureElement = ({ bloodPressure }) => {
+
+    const [editActive, setEditActive] = useState("false");
+    const [id, setElementId] = useState("");
+    const [time, setElementTime] = useState("");
+    const [title, setElementTitle] = useState("");
+    const [unit, setElementUnit] = useState("");
+    const [dose, setElementDose] = useState("");
+
+    const deleteBloodPressure = async (id) => {
+        const bloodPressureDoc = doc(db, "bloodPressure", id);
+        await deleteDoc(bloodPressureDoc);
+    };
+
+    const selectBloodPressure = () => {
+        setEditActive(true);
+        setElementId(bloodPressure.id);
+        setElementTitle(bloodPressure.title);
+        setElementTime(bloodPressure.time);
+        setElementDose(bloodPressure.dose);
+        setElementUnit(bloodPressure.unit);
+        console.log("selectMedi: " + bloodPressure.title);
+    }
+
+    const updateBloodPressure = async (click, id) => {
+        click.preventDefault();
+        const bloodPressureDoc = doc(db, "bloodPressure", id);
+        await updateDoc(bloodPressureDoc, {
+            time: time,
+            title: title,
+            dose: dose,
+            unit: unit
+        });
+        setEditActive(false);
+        setElementTitle("");
+        setElementId("");
+    };
+
+
+    return (
+        <div key={bloodPressure.id} className="medi-list-item">
+
+            <div>
+                <p>{bloodPressure.time.toString()} - {bloodPressure.title} - {/* {bloodPressure.id} */} {bloodPressure.dose} {bloodPressure.unit}</p>
+            </div>
+
+            <div className="list-element-btn-box">
+                {/* <button onClick={() => { deleteMedication(medi.id); }} >
+                    <span className="material-icons-round">
+                        delete
+                    </span>
+                </button> */}
+
+                <button onClick={() => selectBloodPressure()} >
+                    <span className="material-icons-round">settings</span>
+                </button>
+            </div>
+
+
+
+            {
+                editActive === true &&
+                <BloodPressureModal
+                    bloodPressure={bloodPressure}
+                    setEditActive={setEditActive}
+                    setElementTitle={setElementTitle}
+                    title={title}
+                    setElementTime={setElementTime}
+                    time={time}
+                    setElementId={setElementId}
+                    id={id}
+                    setElementDose={setElementDose}
+                    dose={dose}
+                    setElementUnit={setElementUnit}
+                    unit={unit}
+                    updateBloodPressure={updateBloodPressure}
+                    deleteBloodPressure={deleteBloodPressure}
+                />
+            }
+
+        </div>
+    );
+
+}
+
+export default BloodPressureElement; 
